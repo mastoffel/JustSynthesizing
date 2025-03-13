c77e36d03c23b97df470067af567fc745a88808b
Martin Stoffel <martin.adam.stoffel@gmail.com>
2025-03-13 12:22:19 +0000

    add data extractor

--- src/main.py
+++ src/main.py
@@ -1,15 +1,35 @@
 from dotenv import load_dotenv
 from anthropic import Anthropic
+import base64
 
 load_dotenv()
 
-client = Anthropic()
+# read pdf file
+with open("data/input/Weiss_et_al_2017_data_avail.pdf", "rb") as f:
+    pdf_data = base64.standard_b64encode(f.read()).decode("utf-8")
 
+client = Anthropic()
 message = client.messages.create(
-    model="claude-3-haiku-20240307",
-    max_tokens=1000,
+    model="claude-3-5-haiku-20241022",
+    max_tokens=1024,
     messages=[
-        {"role": "user", "content": "Translate the poem Mondnacht to English."}
+        {
+            "role": "user", 
+            "content": [
+                {
+                    "type": "document",
+                    "source": {
+                        "type": "base64",
+                        "media_type": "application/pdf",
+                        "data": pdf_data
+                    }
+                },
+            {
+                "type": "text",
+                    "text": "Make a table of the estimated repeatabilities (ICC's) in the paper, with trait, estimate, ci if available and sample size."
+                }
+            ]
+        }
     ]
 )
 
